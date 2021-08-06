7e02076c51ab858875270ee789f05ce9a98bf054
tainguyen-07 <taitannguyen0808@gmail.com>
2021-08-06 21:43:09 +0700

    second commit

---
+++ Assets/Code/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+     private Rigidbody2D rb;
+     private SpriteRenderer sprite;
+     private Animator animator;
+     private float dirX = 0f;
+     [SerializeField] private float moveSpeed = 7f;
+     [SerializeField] private float jumpForce = 5f;
+    private void Start()
+    {
+        rb =  GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        UpdateAnimation();
+    }
+    private void UpdateAnimation()
+    {
+        if (dirX > 0f)
+        {
+            animator.SetBool("Speed", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            animator.SetBool("Speed", true);
+            sprite.flipX = true;
+        }
+        else 
+        {
+            animator.SetBool("Speed", false);
+        }
+
+    }
+}
+
---
+++ Assets/Code/nv.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class nv : MonoBehaviour{
+    private Transform AimTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAt();
+    }
+
+    private void Awake()
+{ 
+    AimTransform = transform.Find("Aim");
+
+}
+public void LookAt()
+{
+    Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+    float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+    AimTransform.eulerAngles = new Vector3(0, 0, angle);
+}
+public static Vector3 GetMouseWorldPosition()
+{
+    Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    vec.z = 0f;
+    return vec;
+
+}
+public static Vector3 GetMouseWorldPositionWithZ() => GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+{
+    return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+
+}
+public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+{
+    Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+
+    return worldPosition; 
+}
+
+}
